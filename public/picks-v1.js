08b11cf28edb5afa1a2a3ea3953073b71c8536be
realswampdoge-boop <realswampdoge@gmail.com>
2026-02-28 18:05:05 -0500

    Update picks-v1.js

--- public/picks-v1.js
+++ public/picks-v1.js
@@ -141,7 +141,7 @@ async function refreshVipForWallet(wallet) {
     setDebug("Checking SwampDoge...");
     setSwampBal("...");
 
-    const balance = 420000;
+    const balance = await getSwampdogeBalance(wallet);
 
     setSwampBal(balance);
 
